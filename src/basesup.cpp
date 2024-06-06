62ee8f65f09ef19fe2930ddc6f22631751dcb764
wnstngs <64602598+wnstngs@users.noreply.github.com>
2024-06-06 10:57:36 +0300

    [BASESUP] Add basic support routine infrastructure

---
+++ src/basesup.cpp
@@ -0,0 +1,42 @@
+﻿/*!
+ * @file    basesup.cpp
+ * @details Basic support routines for the NtSourceTree project. These may include
+ *          common general purpose algorithms and data structures, logging, exception
+ *          and assertion infrastructure, and more.
+ */
+
+#include "basesup.h"
+
+#include <format>
+#include <typeinfo>
+
+BUF_EXCEPTION::BUF_EXCEPTION()
+{
+}
+
+BUF_EXCEPTION::BUF_EXCEPTION(
+    std::string Message
+) : Message_(std::move(Message))
+{
+}
+
+BUF_EXCEPTION::BUF_EXCEPTION(
+    const std::wstring &Message
+) : Message_(Common::Util::WstringToString(Message))
+{
+}
+
+const char *
+BUF_EXCEPTION::what() const
+{
+    using namespace std::string_literals;
+
+    Buffer_ = "["s + typeid(const_cast<BUF_EXCEPTION &>(*this)).name() + "]"s;
+
+    if (!Message_.empty()) {
+        Buffer_ += ": ";
+        Buffer_ += Message_;
+    }
+
+    return Buffer_.c_str();
+}
---
+++ src/basesup.h
@@ -0,0 +1,34 @@
+﻿/*!
+ * @file    basesup.h
+ * @details Basic support routines for the NtSourceTree project. These may include
+ *          common general purpose algorithms and data structures, logging, exception
+ *          and assertion infrastructure, and more.
+ */
+
+#pragma once
+
+#include <exception>
+#include <string>
+
+class EXCEPTION_BASE : public std::exception {
+};
+
+class BUF_EXCEPTION : public EXCEPTION_BASE {
+public:
+    BUF_EXCEPTION();
+
+    BUF_EXCEPTION(
+        std::string Message
+    );
+
+    BUF_EXCEPTION(
+        const std::wstring &Message
+    );
+
+    const char *
+    what() const override;
+
+private:
+    std::string Message_;
+    mutable std::string Buffer_;
+};
